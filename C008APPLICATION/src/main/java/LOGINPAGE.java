ec7a4ecf487a2c7818e886eedf53538dce0fbf25
thiru <126135933+thiruhyd@users.noreply.github.com>
2023-03-12 14:39:35 +0530

    Add files via upload

---
+++ C008APPLICATION/src/main/java/LOGINPAGE.java
@@ -0,0 +1,10 @@
+
+public class LOGINPAGE {
+
+   public static void main(string [] args} {
+         //TODO Auto-generated method atub
+	     System.out.println("hi good morning") ; 
+	  
+      }
+   
+   }   
